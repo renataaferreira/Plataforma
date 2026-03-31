88332a1e7e57381ac86db14ea3412c22a377c13a
renataaferreira <ferreira.renata@escolar.ifrn.edu.br>
2026-03-31 10:24:44 -0300

    Audios e codigos de AudioManager
    Que Raiva

--- Plataformas-Aula/Assets/Scripts/AudioManager.cs
+++ Plataformas-Aula/Assets/Scripts/AudioManager.cs
@@ -1,22 +1,88 @@
+using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class AudioManager : MonoBehaviour
 {
-   public  static AudioManager Instance;
+    
+    private  AudioSource systemSource;
+    private List<AudioSource> activeSource;
+    
+    public static AudioManager Instance;
 
-   private void Awake()
-   {
-      if (Instance == null)
-      {
-         Instance = this;
-         DontDestroyOnLoad(gameObject);
-      }
-      else
-      {
-         Destroy(gameObject);
-         
-      }
-         
-   }
-   
+    
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+            systemSource = GetComponent<AudioSource>();
+            activeSource = new List<AudioSource>();
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void Play(AudioClip clip)
+    {
+        systemSource.Stop();
+        systemSource.clip = clip;
+        systemSource.Play();
+    }
+
+    public void Stop()
+    {
+        systemSource.Stop();
+    }
+
+    public void Pause()
+    {
+        systemSource.Pause();
+    }
+
+    public void Resume()
+    {
+        systemSource.UnPause();
+    }
+    
+    public void PlayOneShot(AudioClip clip)
+    {
+        systemSource.PlayOneShot(clip);
+    }
+    
+    public void Play(AudioClip clip, AudioSource source)
+    {
+        if(!activeSource.Contains(source))
+            activeSource.Add(source);
+        systemSource.Stop();
+        systemSource.clip = clip;
+        systemSource.Play();
+    }
+
+    public void Stop(AudioSource source)
+    {
+        if(activeSource.Contains(source)) 
+            source.Stop();
+        activeSource.Remove(source);
+    }
+
+    public void Pause(AudioSource source)
+    {
+        if(activeSource.Contains(source)) 
+            source.Pause();
+    }
+
+    public void Resume(AudioSource source)
+    {
+        if(activeSource.Contains(source))
+            source.UnPause();
+    }
+    
+    public void PlayOneShot(AudioClip clip, AudioSource source)
+    {
+        systemSource.PlayOneShot(clip);
+    }
 }
